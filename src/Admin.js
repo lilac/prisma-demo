c3863ef8e5314067c222e6393141ae5b0b77f8b0
lilac <cnjamesdeng@gmail.com>
2018-08-29 14:31:01 +0800

    Add dashboard and customize icons

--- src/Admin.js
+++ src/Admin.js
@@ -3,12 +3,15 @@ import {Admin, Resource} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import {PostList, PostCreate, PostEdit} from './posts';
 import {UserList} from "./users";
+import Dashboard from './dashboard';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () =>
-    <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit}/>
-        <Resource name="users" list={UserList}/>
+    <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+        <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} icon={PostIcon}/>
+        <Resource name="users" list={UserList} icon={UserIcon}/>
     </Admin>;
 
 export default App;
