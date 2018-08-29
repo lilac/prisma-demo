20a19856a6ad42d9f4cd23f40db07c831dc1127c
lilac <cnjamesdeng@gmail.com>
2018-08-29 14:50:31 +0800

    Add a dump authentication provider

--- src/Admin.js
+++ src/Admin.js
@@ -4,12 +4,14 @@ import jsonServerProvider from 'ra-data-json-server';
 import {PostList, PostCreate, PostEdit} from './posts';
 import {UserList} from "./users";
 import Dashboard from './dashboard';
+import authProvider from './authProvider';
+
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () =>
-    <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+    <Admin dataProvider={dataProvider} authProvider={authProvider} dashboard={Dashboard}>
         <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} icon={PostIcon}/>
         <Resource name="users" list={UserList} icon={UserIcon}/>
     </Admin>;
