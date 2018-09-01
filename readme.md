728b3f2eff17842603b62f1639c6f661a508a7ef
lilac <cnjamesdeng@gmail.com>
2018-09-01 10:26:38 +0800

    Add a script to import question answers from csv file

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
+# Prerequisite
+* Install docker and docker-compose
+* Start the prisma service with
+`docker-compose -f prisma/docker-compose.yml up`
+* Install prisma with
+`yarn global add prisma`
+* Deploy data models to prisma with `prisma deploy`
+* Install project dependencies with `yarn`
+
 # Frontend
 To view the demo, run
 ```bash
@@ -14,4 +23,10 @@ node src/server.js
 Open the GraphQL playground with
 ```bash
 graphql playground
+```
+
+# Import csv data
+Import questions and answers in csv to the prisma with the command
+```bash
+node src/script.js AS-4000-train.csv
 ```
--- src/script.js
+++ src/script.js
@@ -1,56 +1,73 @@
-const { Prisma } = require("prisma-binding")
+const {Prisma} = require("prisma-binding");
+const fs = require('fs');
+const papa = require('papaparse');
 
+const env = process.env.NODE_ENV || 'development'
 const prisma = new Prisma({
     typeDefs: "src/generated/prisma.graphql",
-    endpoint: "http://localhost:4466"
+    endpoint: `http://localhost:4466/qa/${env}`
 })
 
-prisma.mutation
-    .createUser({ data: { name: "Alice" } }, "{ id name }")
-    .then(console.log)
-    // { id: 'cjhcidn31c88i0b62zp4tdemt', name: 'Alice' }
-    .then(() => prisma.query.users(null, "{ id name }"))
-    .then(response => {
-        console.log(response)
-        // [ { id: 'cjhcidn31c88i0b62zp4tdemt', name: 'Alice' } ]
-        return prisma.mutation.createPost({
-            data: {
-                title: "Prisma rocks!",
-                content: "Prisma rocks!",
-                author: {
-                    connect: {
-                        id: response[0].id
-                    }
-                }
+function parseCsv(file) {
+    const text = fs.readFileSync(file, 'utf8');
+    const data = papa.parse(text, {header: true})
+    if (data.data) {
+        return data.data
+    } else {
+        throw new Error(data.errors)
+    }
+}
+
+function collectQuestionAnswers(data) {
+    const map = new Map()
+    for (const record of data) {
+        const cls = record['class']
+        if (map.has(cls)) {
+            const r = map.get(cls)
+            r['questions'].push(record['question'])
+        } else {
+            const r = {
+                title: record.question,
+                content: record.answer,
+                questions: [record.question]
+            }
+            map.set(cls, r)
+        }
+    }
+    return map
+}
+
+function qaToMutation(map) {
+    let posts = []
+    for (const entry of map.values()) {
+        posts.push({
+            title: entry.title,
+            content: entry.content,
+            questions: {
+                create: entry.questions.map(title => {
+                    return {title}
+                })
             }
         })
-    })
+    }
+    return {
+        create: posts
+    }
+}
+
+const file = process.argv[2]
+if (!file) process.exit(1)
+
+const data = parseCsv(file)
+const qa = collectQuestionAnswers(data)
+const postMutation = qaToMutation(qa)
+
+prisma.mutation
+    .createAgent({data: {name: "Alice", posts: postMutation}}, "{ id name }")
+    .then(console.log)
+    // { id: 'cjhcidn31c88i0b62zp4tdemt', name: 'Alice' }
+    .then(() => prisma.query.agents(null, "{ id name posts {title content questions {title}} }"))
     .then(response => {
         console.log(response)
-        /*
-          { id: 'cjhcidoo5c8af0b62kv4dtv3c',
-            title: 'Prisma rocks!',
-            content: 'Prisma rocks!',
-            published: false }
-        */
-        return prisma.mutation.updatePost({
-            where: { id: response.id },
-            data: { published: true }
-        })
     })
-    .then(console.log)
-    /*
-      { id: 'cjhcidoo5c8af0b62kv4dtv3c',
-        title: 'Prisma rocks!',
-        content: 'Prisma rocks!',
-        published: true }
-    */
-    .then(() => prisma.query.users(null, "{ id posts { title } }"))
-    .then(console.log)
-    // [ { id: 'cjhcidn31c88i0b62zp4tdemt', posts: [ [Object] ] } ]
-    .then(() => prisma.mutation.deleteManyPosts())
-    .then(console.log)
-    // { count: 1 }
-    .then(() => prisma.mutation.deleteManyUsers())
-    .then(console.log)
-// { count: 1 }
+    .catch(console.error)
